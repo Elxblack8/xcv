a5073a8d7229bc242b8908dc92fd5dfb2340ffa9
Elxblack8 <143453241+Elxblack8@users.noreply.github.com>
2023-08-29 00:09:18 +0300

    Update xc.py

--- xc.py
+++ xc.py
@@ -1,19 +1,9 @@
-#ايجاد الارقام الفرديه
-G= '\033[95m' 
-a= '\033[94m' 
-d= '\033[96m' 
-f= '\033[92m'
-c= '\033[93m' 
-x= '\033[91m' 
-v= '\033[0m' 
-h= '\033[1m' 
-k= '\033[4m' 
-
+import requests
 print(a+"[*]ايجاد الارقام الفرديه\n")
 
-z=int(input(c+"Enter Your First number: "))
+z=int(input("Enter Your First number: "))
 print()
-x=int(input(f+"Enter Your list number: "+d))
+x=int(input("Enter Your list number: "))
 start = z
 end = x
  
